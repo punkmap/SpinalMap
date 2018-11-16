3eee3cfc23528d7324c7a8782e693ff63bee1251
punkmap <tylerdean.w@gmail.com>
2018-11-16 18:38:35 -0500

    these go to 11

--- src/components/SpinalMap.vue
+++ src/components/SpinalMap.vue
@@ -1,6 +1,11 @@
 <template>
   <v-map :zoom=13 :center="[35.791538, -78.781120]" id="map">
-    <v-tilelayer url="https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey=b91af2ae14734b8ca925193df7b940d7"></v-tilelayer>
+    <!-- you need to get a thunderforest.com api key for the full effect or you can just use something like stamen in the line below-->
+    <!--forSpinalMap-->
+    <!--<v-tilelayer url="https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png?apikey=<get thunderforest.com API key to go to 11"></v-tilelayer>-->
+    <!--else you can always be boring and use the following line of code-->
+    <!--<v-tilelayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"></v-tilelayer>-->
+    
     </v-map>
 </template>
 
